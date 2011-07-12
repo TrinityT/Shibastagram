557e81da0f4b7c200b881d90507225ebc63d54dd
takakura <takakura@D1020129.(none)>
2011-07-12 16:43:19 +0900

    コミット

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,8 @@
 # Client ID
-CLIENT_ID = ''
+CLIENT_ID = 'f2bc82ac62fa4c79997b934044ba0238'
 
 # Cron token
 CRON_TOKEN = ''
 
-# Target tags ['shiba', 'shibainu', 'shibadog', 'shibas']
-TARGET_TAGS = []
-
+# Target tags
+TARGET_TAGS = ['flower', 'hana']
